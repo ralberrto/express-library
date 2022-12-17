a651bf6e7a1a74f5cbc7121aade380f1ee087491
Rafael Alberto Larios <albertolarios@me.com>
2022-12-17 16:02:52 -0600

    Add MongoDB schemas corresponding to the different collections

---
+++ models/author.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+    first_name: { type: String, required: true, maxLength: 100 },
+    family_name: { type: String, required: true, maxLendth: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+
+    virtuals: {
+        name: {
+            get() {
+                let fullname = '';
+                if (this.first_name && this.family_name) {
+                    fullname = `${this.family_name}, ${this.first_name}`;
+                }
+                return fullname;
+            },
+        url: {
+            get() {
+                return `/catalog/author/${this._id}`;
+            }
+        }
+        }
+    },
+});
+
+// Alternative way to define virtual properties
+//AuthorSchema.virtual('name').get(function());
+
+module.exports = mongoose.model('Author', AuthorSchema);
