6ed600af32837b201c133fcbd2fa8a61cbd0feb7
Rafael Alberto Larios <albertolarios@me.com>
2022-12-20 09:20:07 -0600

    Implement the genre detail page and associated books

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,8 @@
 const Genre = require('../models/genre');
+const Book = require('../models/book');
+
+const async = require('async');
+
 
 exports.genre_list = (req, res, next) => {
     Genre.find()
@@ -9,8 +13,29 @@ exports.genre_list = (req, res, next) => {
         });
 };
 
-exports.genre_detail = (req, res) => {
-    res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+exports.genre_detail = (req, res, next) => {
+    async.parallel({
+        genre(callback) {
+            Genre.findById(req.params.id).exec(callback);
+        },
+        genre_books(callback) {
+            Book.find({ genre: req.params.id })
+                .sort({ title: 1 })
+                .exec(callback);
+        },
+    }, (err, results) => {
+        if (err) return next(err);
+        if (!results.genre) {
+            const err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('genre_detail', {
+            title: 'Genre Detail',
+            genre: results.genre,
+            genre_books: results.genre_books,
+        });
+    });
 };
 
 exports.genre_create_get = (req, res) => {
